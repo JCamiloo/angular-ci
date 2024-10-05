9e1bcd8b410d246efe6c74a96efc8d105e2684af
JCamiloo <jcamilo.osorio15@gmail.com>
2024-10-05 15:23:42 -0500

    add conditionals

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -17,13 +17,15 @@ describe('AppComponent', () => {
   it(`should have the 'angular-ci' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('angular-ci');
+    expect(app.title).toEqual('angular-ci2');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, angular-ci');
+    expect(compiled.querySelector('h1')?.textContent).toContain(
+      'Hello, angular-ci2s'
+    );
   });
 });
